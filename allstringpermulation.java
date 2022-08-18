d775a92ffdc00ab162ae4756eccda67fc188632b
csamanta2000 <107202988+csamanta2000@users.noreply.github.com>
2022-08-18 12:15:18 +0530

    thusday practical 18-08-2022

---
+++ allstringpermulation.java
@@ -0,0 +1,38 @@
+package codes;
+
+//Java program to print all the permutations
+
+public class allstringpermulation {
+
+
+	static void printPermutn(String str, String ans)
+	{
+
+	
+		if (str.length() == 0) {
+			System.out.print(ans + " ");
+			return;
+		}
+
+		for (int i = 0; i < str.length(); i++) {
+
+		
+			char ch = str.charAt(i);
+
+		
+			String ros = str.substring(0, i) +
+						str.substring(i + 1);
+
+	
+			printPermutn(ros, ans + ch);
+		}
+	}
+
+	
+	public static void main(String[] args)
+	{
+		String s = "chandan";
+		printPermutn(s, "");
+	}
+}
+
---
+++ testrevstring.java
@@ -0,0 +1,8 @@
+package codes;
+
+public class testrevstring {  
+public static void main(String[] args) {  
+    System.out.println(revstring.reverseWord("my name is chandan"));  
+    System.out.println(revstring.reverseWord("I love to ride bike"));    
+    }  
+}  
